b3ae41da12ca3376e63f61ea348247f622e66b7f
daulet2030 <daulet2030@gmail.com>
2023-11-19 22:58:24 +0600

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cloudflare-angular';
+  constructor(private http: HttpClient) {}
+
+  onApiRequest() {
+    this.http
+      .get('https://google.com')
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
